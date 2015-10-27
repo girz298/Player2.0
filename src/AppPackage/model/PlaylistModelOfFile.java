9f527e649fe29bba0880a7f2ddd610a96cb24f95
girz298 <girz298@gmail.com>
2015-10-27 23:10:08 +0200

    23123213

--- src/AppPackage/model/PlaylistModelOfFile.java
+++ src/AppPackage/model/PlaylistModelOfFile.java
@@ -1,16 +1,24 @@
 
 package AppPackage.model;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PlaylistModelOfFile {
-   public ArrayList<String> playlistOfSongs =new ArrayList<>();
-
- 
+     private BufferedReader BufferedFileInputStream;    
+     private StringBuffer stringBuff;
+     private String str;
+     private List<ModelOfFile> listOfPatFilesh=new ArrayList<ModelOfFile>();
+     private String stopString;
+     
+     
     
     public void savePlaylist(String fileName, List<ModelOfFile> listOfPaths)
     {
@@ -27,7 +35,7 @@ public class PlaylistModelOfFile {
         try {
             for(int i = 0;i<listOfPaths.size();i++)
             {
-            out.print(listOfPaths.get(i).getPath()+";"+"\r\n");
+            out.print(listOfPaths.get(i).getPath()+";"/*+"\r\n"*/);
             }
         } finally {
             //После чего мы должны закрыть файл
@@ -38,6 +46,44 @@ public class PlaylistModelOfFile {
         throw new RuntimeException(e);
     }
     
+    }
+    
+    public List<ModelOfFile> openPlayList(String pathOfPlayList) throws IOException
+    {
+        listOfPatFilesh.removeAll(listOfPatFilesh);
+        int end;
+        ModelOfFile modelOfFile;
+        int nameID=0;
+       BufferedFileInputStream = new BufferedReader(new InputStreamReader(new FileInputStream(pathOfPlayList)));
+        while((str=BufferedFileInputStream.readLine())!=null)
+            stringBuff = new StringBuffer(str);
+        BufferedFileInputStream.close();
+        
+        
+         while(true){
+             try {
+                 end=stringBuff.indexOf(";");
+             modelOfFile=new ModelOfFile();
+             modelOfFile.setPath(stringBuff.subSequence(0, end).toString());
+             for(int i=end;i>0;i--){
+                 if(stringBuff.charAt(i)=='\\'){
+                 nameID=i;
+                 break;
+                 }
+                 
+             }
+             modelOfFile.setName(stringBuff.subSequence(nameID+1, end).toString().replaceFirst(".mp3", ""));
+             listOfPatFilesh.add(modelOfFile);
+               
+             stringBuff.delete(0, end+1);
+             } catch (Exception e) {
+                 break;
+             }
+             
+             
+         }
+                System.out.println("dsikj");
+        return listOfPatFilesh;
     }
   
     
